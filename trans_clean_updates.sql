5aed9e9a5d54d47f4b97e7f43e81ec97299feaa7
Sim Kennedy <sim__k@hotmail.com>
2015-09-11 15:09:44 +0100

    Update sss insert to usm3 to use tc_slim and moved query to bottom. Also  added timestamps for checking logs.

--- trans_clean_updates.sql
+++ trans_clean_updates.sql
@@ -1,7 +1,7 @@
-﻿-- Start time
-SELECT CURRENT_TIMESTAMP;
-
+﻿
 -- Updates the supplier_id of any files posted today and the previous day
+
+SELECT 'Updating trans_clean supplier_ids.', CURRENT_TIMESTAMP;
 
 UPDATE trans_clean
 SET supplier_id = usm3.sid
@@ -12,20 +12,10 @@ AND trans_clean.supplier_id is null
 AND trans_clean.created_at > now()::date - 1
 ;
 
--- Updates usm3 with new supplier_source_strings added to trans_clean in the last 2 days.
-
-INSERT INTO usm3 (sss, sss_created_at)
-SELECT DISTINCT ON (supplier_source_string) supplier_source_string, CURRENT_DATE
-FROM trans_clean
-WHERE supplier_id is null
-AND supplier_source_string ~* '[A-Z]'
-AND updated_at > (CURRENT_DATE - 2)
-AND NOT EXISTS (SELECT sss FROM usm3 WHERE sss = trans_clean.supplier_source_string)
-ORDER BY supplier_source_string ASC
-RETURNING sss, sss_created_at
-;
 
 -- Capgemini into hmrc
+
+SELECT 'Updating trans_clean - Capgemini into hmrc.', CURRENT_TIMESTAMP;
 
 UPDATE trans_clean
 SET supplier_id = '00943935_com'
@@ -36,7 +26,10 @@ AND trans_clean.supplier_id is null
 AND trans_clean.created_at > now()::date - 10
 ;
 
+
 -- Updates the entity index in trans_clean
+
+SELECT 'Updating entity index.', CURRENT_TIMESTAMP;
 
 UPDATE 
 trans_clean
@@ -45,9 +38,28 @@ entity_pk=e.pk from (select entity_id, pk from entity) as e
 WHERE 
 entity_pk is null AND
 trans_clean.entity_id=e.entity_id
+RETURNING entity_pk, e.entity_id
 ;
+
+
+SELECT 'Refreshing tc_slim.', CURRENT_TIMESTAMP;
 
 REFRESH MATERIALIZED VIEW tc_slim;
 
+
+-- Updates usm3 with new supplier_source_strings added to trans_clean in the last 2 days.
+
+SELECT 'Inserting new sss into usm3', CURRENT_TIMESTAMP;
+
+INSERT INTO usm3 (sss, sss_created_at)
+SELECT DISTINCT ON (supplier_source_string) supplier_source_string, CURRENT_DATE
+FROM tc_slim
+WHERE supplier_id is null
+AND supplier_source_string ~* '[A-Z]'
+AND NOT EXISTS (SELECT 1 FROM usm3 WHERE sss = supplier_source_string)
+RETURNING sss, sss_created_at
+;
+
+
 -- End time
-SELECT CURRENT_TIMESTAMP;
+SELECT 'Finished', CURRENT_TIMESTAMP;
